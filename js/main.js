2dee54a2c8bf2a21ac73b71806e80d85a79d022c
FakhriddinovSh <shaxobiddinfaxriddinov4@gmail.com>
2022-08-26 23:21:25 +0500

    Home page ended

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-// $('.autoplay').slick({
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     autoplaySpeed: 2000,
-// });
-
 const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
 const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+
+
+$('.slider-list').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+          breakpoint: 798,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 2
+          }
+        }
+    ]
+  });
+  
